89a3e831efde93126a7b8e2554194453907696c6
jeffyyoung@gmail.com <jeffyyoung@gmail.com>
2015-01-23 16:35:09 +0800

    近邻初始版本

---
+++ jinlin-admin-api/src/main/java/com/jlinfo/admin/user/facade/AnotherUserRestService.java
@@ -0,0 +1,29 @@
+package com.jlinfo.admin.user.facade;
+
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.jlinfo.admin.user.User;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author lishen
+ */
+@Path("u")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+public interface AnotherUserRestService {
+
+    @GET
+    @Path("{id : \\d+}")
+    User getUser(@PathParam("id") Long id);
+
+    @POST
+    @Path("register")
+    RegistrationResult registerUser(User user);
+}
