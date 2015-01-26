3878c73ac081df89c1d0ef352d69f5b7cb826306
jeffyyoung@gmail.com <jeffyyoung@gmail.com>
2015-01-26 16:14:36 +0800

    忽略项目信息

--- jinlin-admin-dao/src/test/java/com/jlinfo/admin/demo/dao/RestClient.java
+++ jinlin-admin-dao/src/test/java/com/jlinfo/admin/demo/dao/RestClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.dubbo.demo.consumer;
+package com.jlinfo.admin.demo.dao;
 
 import com.jlinfo.admin.user.User;
 
